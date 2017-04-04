e394a7e27d234ecdee102cdbf7f2f2c45cc98e6d
gradyShepherd <gradyshepherd13@gmail.com>
2017-04-03 19:26:34 -0600

    added update panel to comment section

--- WaffleTruck/Site.Master.designer.cs
+++ WaffleTruck/Site.Master.designer.cs
@@ -103,6 +103,15 @@ namespace WaffleTruck {
         protected global::System.Web.UI.WebControls.ContentPlaceHolder MainContent;
         
         /// <summary>
+        /// commentUpdatePanel control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.UpdatePanel commentUpdatePanel;
+        
+        /// <summary>
         /// ListView1 control.
         /// </summary>
         /// <remarks>
